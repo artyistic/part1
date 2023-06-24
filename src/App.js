23512b97d1b0e6a8f322106ada84529bcbe5bfa4
artyistic <artyistic@gmail.com>
2023-06-23 23:28:39 -0400

    part 1b

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Header = (props) =>{
+  console.log("header componenet was used!")
+  return <h1>{props.course.name}</h1>
+}
+
+const Content = (props) =>{
+  console.log("Content componenet was used!")
+  return  [
+            <p> {props.course.parts[0].name} {props.course.parts[0].exercises}</p>,
+            <p> {props.course.parts[1].name} {props.course.parts[1].exercises}</p>,
+            <p> {props.course.parts[2].name} {props.course.parts[2].exercises}</p>
+          ]
+}
+
+const Total = (props) =>{
+  console.log("total componenet was used!")
+  return <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+}
+
+export default App
